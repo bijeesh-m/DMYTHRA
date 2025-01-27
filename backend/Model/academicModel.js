da877faa64f640bf70d16a70713b59262298bf32
Bijeesh M <bijeeshh.m@gmail.com>
2025-01-27 14:38:56 +0530

    First commit

---
+++ backend/Model/academicModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const academicSchema = new mongoose.Schema({
+  videoName: {
+    type: String,
+    required: true,
+  },
+  videoDescription: {
+    type: String,
+    required: true,
+  },
+  videoLink: {
+    type: String,
+    required: true,
+  },
+  videoKey: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = new mongoose.model("AcademicDetails", academicSchema);
